996771f50ea71d29a2382206f4c22bc00dab9fa9
Juliana Connors <65951445+julieconnors@users.noreply.github.com>
2021-09-07 15:23:49 -0400

    add create card view controller with outlets to storyboard

--- Flashcards/CreateCardViewController.swift
+++ Flashcards/CreateCardViewController.swift
@@ -9,21 +9,16 @@ import UIKit
 
 class CreateCardViewController: UIViewController {
 
+    @IBOutlet weak var createButton: UIButton!
+    @IBOutlet weak var headline: UITextField!
+    @IBOutlet weak var cardDescription: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+       
     }
     
-
-    /*
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
