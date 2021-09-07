68d771c79dff8da465186c7d54b87105c68c2f3d
Juliana Connors <65951445+julieconnors@users.noreply.github.com>
2021-09-07 15:22:52 -0400

    add create view with ui objects and constraints

---
+++ Flashcards/CreateCardViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CreateCardViewController.swift
+//  Flashcards
+//
+//  Created by Julie Connors on 9/7/21.
+//
+
+import UIKit
+
+class CreateCardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
