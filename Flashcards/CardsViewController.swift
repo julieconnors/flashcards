b8cc64197ed13884d2153953df1b2849cf73bc45
Juliana Connors <65951445+julieconnors@users.noreply.github.com>
2021-09-08 15:13:09 -0400

    set up collection view

--- Flashcards/CardsViewController.swift
+++ Flashcards/CardsViewController.swift
@@ -8,18 +8,23 @@
 import UIKit
 
 class CardsViewController: UIViewController {
-
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    let cards = ["generics", "protocols", "structs"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBlue
+        configureCollection()
     }
     
-//    func configureCollection() {
-//            collectionView.dataSource = self
-//            collectionView.delegate = self
-//            collectionView.register(UINib(nibName: MyCell.identifier, bundle: nil), forCellWithReuseIdentifier: MyCell.identifier)
-//        }
+    func configureCollection() {
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(UINib(nibName: CardCell.identifier, bundle: nil), forCellWithReuseIdentifier: CardCell.identifier)
+    }
 }
+
 extension CardsViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         //viewModel.numItems
@@ -29,28 +34,18 @@ extension CardsViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         ///create custom cell
-        let cell = UICollectionViewCell()
-//        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MyCell.identifier, for: indexPath) as? MyCell else {
-//            fatalError()
-//        }
-        
-        cell.backgroundColor = .red
-//        cell.topLabel.text = viewModel.getCellData(at: indexPath.row)
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CardCell.identifier, for: indexPath) as? CardCell else {
+            fatalError()
+        }
+                
         return cell
     }
-    
 }
 
 extension CardsViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         
-        switch indexPath.row % 4 {
-        case 0:
-            return CGSize(width: 310, height: 50)
-        default:
-            return CGSize(width: 90, height: 100)
-        }
-        
+        return CGSize(width: 90, height: 100)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
