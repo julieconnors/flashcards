57c1a09a37b16ce0e16916ab036b80ece1ddf923
Juliana Connors <65951445+julieconnors@users.noreply.github.com>
2021-09-09 13:59:50 -0400

    pass description to popup and add delete button

--- Flashcards/CardBackViewController.swift
+++ Flashcards/CardBackViewController.swift
@@ -8,16 +8,39 @@
 import UIKit
 
 class CardBackViewController: UIViewController {
+    //view model
+    
+    var viewModel: CardCollectionViewModel?
+    
+    var labelDescription: String = "" {
+        didSet {
+            descriptionLabel.text = labelDescription
+        }
+    }
+    
+    var deleteButton: UIButton = {
+        let button = UIButton(frame: CGRect(x: 10, y: 10, width: 100, height: 50))
+        button.setTitle("Delete", for: .normal)
+        button.addTarget(self, action: #selector(removeCard), for: .touchUpInside)
+        return button
+    }()
+    
+    @objc func removeCard() {
+        viewModel = CardCollectionViewModel()
+        
+        print("delete card")
+    }
     
     var descriptionLabel: UILabel = {
-        var label = UILabel(frame: CGRect(x: 100, y: 200, width: 100, height: 100))
-        label.text = "Description"
+        var label = UILabel(frame: CGRect(x: 100, y: 200, width: 200, height: 100))
+        label.numberOfLines = 0
         return label
     }()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .orange
         view.addSubview(descriptionLabel)
+        view.addSubview(deleteButton)
     }
 }
